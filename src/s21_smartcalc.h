093484d6cfffc2b7b72158594941ae31bb08d9d5
Иван Чамкин <chamkinivan39@gmail.com>
2023-05-31 00:03:38 +0300

    feat: common project structure

---
+++ src/s21_smartcalc.h
@@ -0,0 +1,7 @@
+#ifndef C7_SMARTCALC_V1_0_1_S21_SMARTCALC_H
+#define C7_SMARTCALC_V1_0_1_S21_SMARTCALC_H
+
+#include "calculate/s21_calculate.h"
+#include "utils/s21_utils.h"
+
+#endif //C7_SMARTCALC_V1_0_1_S21_SMARTCALC_H
