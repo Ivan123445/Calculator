2e4f39b65a183ac9592b7725207c58b534265555
Иван Чамкин <chamkinivan39@gmail.com>
2023-07-16 00:43:21 +0300

    feat: add deposit calc

---
+++ src/gui/deposit_calc_window.c
@@ -0,0 +1,20 @@
+#include "../s21_smartcalc.h"
+
+G_MODULE_EXPORT void calculate_deposit(GtkButton *button, gpointer input_grid) {
+  GtkWidget *sum = gtk_grid_get_child_at(input_grid, 0, 0);
+  GtkWidget *result = gtk_grid_get_child_at(input_grid, 1, 0);
+  GtkWidget *interest_rate = gtk_grid_get_child_at(input_grid, 0, 1);
+
+  char *str_sum = (char *)gtk_entry_get_text(sum);
+  char *str_interest_rate = (char *)gtk_entry_get_text(interest_rate);
+
+  if (strlen(str_sum) && strlen(str_interest_rate)) {
+    float f_sum, f_interest_rate;
+    char str_result[30];
+    sscanf(str_sum, "%f", &f_sum);
+    sscanf(str_interest_rate, "%f", &f_interest_rate);
+
+    sprintf(str_result, "%f", f_sum * f_interest_rate / 100);
+    gtk_entry_set_text(result, str_result);
+  }
+}
--- src/gui/main_window.c
+++ src/gui/main_window.c
@@ -1,34 +1,29 @@
-#include "s21_smartcalc.h"
+#include "../s21_smartcalc.h"
 
-static GtkWidget *create_window(void) {
-  /* это виджет окна */
+static GtkWidget *create_main_window(void) {
   GtkWidget *window;
-  /* это тот волшебный объект, который сделает за нас окошко */
   GtkBuilder *builder;
-  /* сюда будем складывать ошибки */
   GError *error = NULL;
 
-  /* тут загружаем файл с интерфейсом */
   builder = gtk_builder_new();
   if (!gtk_builder_add_from_file(builder, "../src/gui/calc.glade", &error)) {
-    /* загрузить файл не удалось */
     g_critical("Не могу загрузить файл: %s", error->message);
     g_error_free(error);
   }
-
-  /* помните, мы подключали сигналы вручную? теперь это происходит
-   * автоматически! */
   gtk_builder_connect_signals(builder, NULL);
 
-  /* получаем виджет окна, чтобы его показать */
-  window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "Main"));
   if (!window) {
-    /* что-то не так, наверное, ошиблись в имени */
     g_critical("Ошибка при получении виджета окна");
   }
   g_object_unref(builder);
 
   return window;
+}
+
+G_MODULE_EXPORT GtkWidget *create_credit_calc_window(
+    GtkButton *button, GtkWidget *credit_calc_window) {
+  gtk_widget_show(credit_calc_window);
 }
 
 G_MODULE_EXPORT void add_symb_to_str(GtkButton *button, gpointer label) {
@@ -160,19 +155,14 @@ void create_graph_window() {
   }
 }
 
-/* это главная функция нашего приложения, которая будет выполнена первой */
 int main(int argc, char *argv[]) {
-  /* виджет окна */
   GtkWidget *window;
 
-  /* запускаем GTK+ */
   gtk_init(&argc, &argv);
 
-  /* вызываем нашу функцию для создания окна */
-  window = create_window();
+  window = create_main_window();
   gtk_widget_show(window);
 
-  /* передаём управление GTK+ */
   gtk_main();
   return 0;
 }
